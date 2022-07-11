1270ba33fd682ad8041d78d096c4b741d70ff258
david361215 <1138890119@qq.com>
2022-07-11 19:22:34 +0800

    初始化项目

---
+++ david-ui-1/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import Frank from './components/Frank.vue'
+</script>
+
+<template>
+  <div>hi</div>
+  <Frank/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
