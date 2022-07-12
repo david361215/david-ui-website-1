2387be8d86180ebb5389973cdf915d4cfc91551d
david361215 <1138890119@qq.com>
2022-07-12 09:15:19 +0800

    初始化 vue-router

--- david-ui-1/src/App.vue
+++ david-ui-1/src/App.vue
@@ -3,8 +3,11 @@
 </script>
 
 <template>
-  <div>hi</div>
-  <Frank/>
+  <div>导航栏 | 
+    <router-link to="/">Frank</router-link> | 
+    <router-link to="/xxx">Frank2</router-link></div>
+  <hr>
+  <router-view/>
 </template>
 
 <style>
