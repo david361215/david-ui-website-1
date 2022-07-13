490a0c56f7078d355f07560aa10623a26ebd04ef
david361215 <1138890119@qq.com>
2022-07-13 18:30:06 +0800

    优化代码

--- david-ui-1/src/lib/Switch.vue
+++ david-ui-1/src/lib/Switch.vue
@@ -5,19 +5,19 @@
 </template>
 
 <script lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
     props:{
         value:Boolean
     },
 
     setup(props, context){
-        const toggle = ()=>{
-            context.emit('update:value',!props.value)
-        }
-        return {toggle}
-    } 
-}
+        const toggle = () => {
+            context.emit("update:value",!props.value);
+        };
+        return { toggle };
+    },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -30,30 +30,27 @@ button {
   border-radius:calc($h/2);
   background: #bfbfbf; 
   position: relative;
- }
-span {
-  position: absolute; 
-  top: 2px; 
-  left: 2px; 
-  height: $h2; 
-  width: $h2; 
-  background: white; 
-  border-radius:calc($h/2);
-  transition: all 250ms;
-}
-button.checked{
-  background: #1890ff;
-}
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button:focus{
-  outline: none;
-}
-button:active{
-  > span {width: $h2 + 4px;}
-}
-button.checked:active{
-  > span {width: $h2 + 4px; margin-left: -4px;}
+  > span {
+    position: absolute; 
+    top: 2px; 
+    left: 2px; 
+    height: $h2; 
+    width: $h2; 
+    background: white; 
+    border-radius:calc($h/2);
+    transition: all 250ms;
+  }
+  &.checked{ background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus{ outline: none; }
+  &:active{
+    > span {width: $h2 + 4px;}
+  }
+  &.checked:active{
+    > span {width: $h2 + 4px; margin-left: -4px;}
+  }
 }
 </style>
