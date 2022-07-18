2928e04ae72146557934ce58aae665f5a17b2a8e
david361215 <1138890119@qq.com>
2022-07-18 12:19:34 +0800

    支持 level 属性

--- david-ui-1/src/lib/Button.vue
+++ david-ui-1/src/lib/Button.vue
@@ -17,14 +17,19 @@ export default {
       type: String,
       default: "normal",
     },
+    level: {
+      type: String,
+      default: "normal",
+    },
   },
   setup(props) {
-    const { theme, size } = props;
+    const { theme, size, level } = props;
 
     const classes = computed(() => {
       return {
         [`gulu-theme-${theme}`]: theme,
         [`gulu-size-${size}`]: size,
+        [`gulu-level-${level}`]: level,
       };
     });
     return { classes };
@@ -37,6 +42,7 @@ $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
+$red: red;
 $radius: 4px;
 .gulu-button {
   box-sizing: border-box;
@@ -52,6 +58,8 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
+
   & + & {
     margin-left: 8px;
   }
@@ -66,6 +74,28 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.gulu-theme-button {
+    &.gulu-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
   &.gulu-theme-link {
     border-color: transparent;
     box-shadow: none;
@@ -73,6 +103,13 @@ $radius: 4px;
     &:hover,
     &:focus{
       color: lighten($blue, 10%);
+    }
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
   &.gulu-theme-text {
@@ -82,6 +119,20 @@ $radius: 4px;
     &:hover,
     &:focus{
       background: darken(white, 5%);;
+    }
+    &.gulu-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
   &.gulu-size-big{
@@ -94,6 +145,7 @@ $radius: 4px;
     height: 20px;
     padding: 0 4px;
   }
+
 }
 
 </style> 
