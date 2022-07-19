f5f2c3a69af98c66da1a5787c537e43b1d3cee73
david361215 <1138890119@qq.com>
2022-07-19 16:34:16 +0800

    openDialog 一句话打开 Dialog

--- david-ui-1/src/components/DialogDemo.vue
+++ david-ui-1/src/components/DialogDemo.vue
@@ -1,7 +1,6 @@
 <template>
     <div>Dialog 示例</div>
     <h1>示例1</h1>
-    <div style="position: relative; z-index: 1;">
         <Button @click="toggle">toggle</Button>
         <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
             <template v-slot:content>
@@ -12,14 +11,18 @@
                 <strong>加粗的标题</strong>            
             </template>
         </Dialog>
-    </div>
-    <div style="position: relative; z-index: 2; width: 800px; height: 300px; background: red;"></div>
+    <h1>示例2</h1>
+    <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
 import Button from '../lib/Button.vue'
-import { ref } from 'vue'
+import { 
+    ref,
+    h
+} from 'vue'
+import { openDialog } from '../lib/openDialog'
 
 export default{
     components: {
@@ -35,11 +38,24 @@ export default{
             return false
         }
         const f2 = () =>{}
+        const showDialog = () => {
+            openDialog({
+                title: h('strong', {}, '标题'),
+                content: '你好',
+                ok() {
+                    console.log('ok');
+                },
+                cancel() {
+                    console.log('cancel');
+                }
+            })
+        }
         return {
             x,
             toggle,
             f1,
-            f2
+            f2,
+            showDialog
         }
     }
 }
