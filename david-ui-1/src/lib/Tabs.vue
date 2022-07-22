bef287da8710d991e3b7e5e59bacb0f9f6d1e998
david361215 <1138890119@qq.com>
2022-07-22 11:50:56 +0800

    实现下划线滑动

--- david-ui-1/src/lib/Tabs.vue
+++ david-ui-1/src/lib/Tabs.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="gulu-tabs">
-        <div class="gulu-tabs-nav">
-            <div class="gulu-tabs-nav-item" :class="{selected: t===selected}" @click="select(t)" v-for="(t,index) in titles" :key="index" >{{t}}</div>
+        <div class="gulu-tabs-nav" ref="container">
+            <div class="gulu-tabs-nav-item" :class="{selected: t===selected}" ref="navItems" @click="select(t)" v-for="(t,index) in titles" :key="index" >{{t}}</div>
+            <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
         </div>
         <div class="gulu-tabs-content">
             <component class="gulu-tabs-content-item" :class="{ selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
@@ -12,7 +13,10 @@
 <script lang="ts">
 import Tab from "./Tab.vue";
 import {
-    computed
+    computed,
+    ref,
+    onMounted,
+    onUpdated
 } from "vue";
 
 export default {
@@ -22,6 +26,29 @@ export default {
         }
     },
     setup(props,context) {    
+        const navItems = ref<HTMLDivElement[]>([])
+        const indicator = ref<HTMLDivElement>(null)
+        const container = ref<HTMLDivElement>(null)
+        const x = () => {
+            const divs = navItems.value;
+            const result = divs.find(div => div.classList.contains('selected'))
+            // const { 
+            //     width 
+            // } = result.getBoundingClientRect()
+            const width = result.style.width
+            indicator.value.style.width = width + 'px'
+            const { 
+                left: left1 
+            } = container.value.getBoundingClientRect()
+            const { 
+                left: left2 
+            } = result.getBoundingClientRect()
+            const left = left2 - left1
+            indicator.value.style.left = left + 'px'
+        }
+        onMounted(x)
+        onUpdated(x)
+
         const defaults = context.slots.default()
         defaults.forEach((tag) => {
             if(tag.type !== Tab){
@@ -45,7 +72,10 @@ export default {
             defaults,
             titles,
             select,
-            current
+            current,
+            navItems,
+            indicator,
+            container
         }
     }
 }
@@ -61,6 +91,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -71,6 +102,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+        position: absolute;
+        left: 0;
+        bottom: -1px;
+        width: 100px;
+        height: 3px;
+        background: $blue;
+        transition: all 250ms;
     }
   }
   &-content {
