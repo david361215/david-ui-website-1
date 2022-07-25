886fb5569fd4521fccbcf553f248963d97200609
david361215 <1138890119@qq.com>
2022-07-25 11:38:12 +0800

    修改背景色、链接样式

--- david-ui-1/src/components/Topnav.vue
+++ david-ui-1/src/components/Topnav.vue
@@ -1,30 +1,36 @@
 <template>
-    <div class="topnav">
-        <div class="logo">LOGO</div>
-        <ul class="menu">
-            <li>菜单1</li>
-            <li>菜单2</li>
-        </ul>
-        <span class="toggleAside" @click="toggleAside"></span>
-    </div>
+<div class="topnav">
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+    </ul>
+    <span class="toggleAside" @click="toggleAside"></span>
+</div>
 </template>
 
 <script lang="ts">
-    import {inject,Ref} from 'vue'
+    import {
+      inject,
+      Ref
+    } from 'vue'
     export default{
         setup() {
-            const asideVisible = inject<Ref<boolean>>('asideVisible')
-            const toggleAside = ()=>{
+            const asideVisible = inject < Ref < boolean >> ('asideVisible')
+            const toggleAside = () => {
                 asideVisible.value = !asideVisible.value
             }
-            return{toggleAside}
+            return {
+              toggleAside
+            }
         }
     }
 </script>
 
 <style lang="scss" scoped>
+$color: #007974;
+
 .topnav {
-  background: pink;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -34,19 +40,23 @@
   z-index: 10;
   justify-content: center;
   align-items: center;
-  > .logo {
+  color: $color;
+  >.logo {
     max-width: 6em;
     margin-right: auto;
   }
-  > .menu {
+
+  >.menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
-    > li {
+
+    >li {
       margin: 0 1em;
     }
   }
-  > .toggleAside{
+
+  >.toggleAside{
     display: none;
     width: 24px;
     height: 24px;
@@ -56,17 +66,19 @@
     top:50%;
     transform: translateY(-50%);
   }
-  @media(max-width:500px){
-  > .menu{
-    display: none;
-  }
-  > .logo{
-    margin:0 auto;
-  }
-  > .toggleAside{
-    display: inline-block;
-  }
-}
-}
 
+  @media(max-width:500px){
+    >.menu{
+      display: none;
+    }
+  
+    >.logo{
+      margin:0 auto;
+    }
+
+    >.toggleAside{
+      display: inline-block;
+    }
+  }
+}
 </style>
--- david-ui-1/src/views/Home.vue
+++ david-ui-1/src/views/Home.vue
@@ -1,43 +1,57 @@
 <template>
-    <div>
-        <Topnav/>
-        <div class="banner">
-            <h1>david UI</h1>
-            <h2>一个厉害的 UI 框架</h2>
-            <p class="actions">
-                <a href="https://github.com">Github</a>
-                <router-link to="/doc">开始</router-link>
-            </p>
-        </div>
+<div class="topnavAndBanner">
+    <Topnav/>
+    <div class="banner">
+        <h1>david UI</h1>
+        <h2>一个厉害的 UI 框架</h2>
+        <p class="actions">
+            <a href="https://github.com">Github</a>
+            <router-link to="/doc">开始</router-link>
+        </p>
     </div>
+</div>
 </template>
 
 <script lang="ts">
     import Topnav from '../components/Topnav.vue'
     export default{
-        components:{Topnav}
-    }
+        components: {
+          Topnav
+        },
+    };
 </script>
 
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+
+.topnavAndBanner{
+  background: linear-gradient(145deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
+}
+
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
-  > .actions{
+  color: $color;
+
+  >.actions {
     padding: 8px 0;
+
     a {
-      margin:0 8px;
-      background: #fff;
+      margin: 0 8px;
+      background: $green;
+      color: white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: calc($h/2);
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+
+      &:hover{
+        text-decoration: none;
+      }
     }
   }
 }
