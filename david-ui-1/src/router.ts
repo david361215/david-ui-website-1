5f994958f9550525b75fb5600ba56ab3a00e1921
david361215 <1138890119@qq.com>
2022-08-01 11:58:16 +0800

    事不过三：优化 markdown

--- david-ui-1/src/router.ts
+++ david-ui-1/src/router.ts
@@ -6,12 +6,11 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
-
-import Intro from './views/Intro.vue';
-import GetStarted from './views/GetStarted.vue';
-import Install from './views/Install.vue';
+import Markdown from './components/Markdown.vue'
+import { h } from 'vue'
 
 const history = createWebHashHistory()
+const md = filename => h(Markdown, {path: `../markdown/${filename}.md`, key: filename})
 export const router = createRouter({
     history:history,
     routes:[
@@ -23,29 +22,29 @@ export const router = createRouter({
             component:Doc,
             children:[
                 {
-                    path:"",
-                    component:DocDemo 
+                    path: "",
+                    component: DocDemo
                 },{
-                    path:"intro",
-                    component:Intro
+                    path: "intro",
+                    component: md('intro')
                 },{
-                    path:"get-started",
-                    component:GetStarted
+                    path: "get-started",
+                    component: md('get-started')
                 },{
-                    path:"install",
-                    component:Install
+                    path: "install",
+                    component: md('install')
                 },{
-                    path:'switch',
-                    component:SwitchDemo
+                    path: 'switch',
+                    component: SwitchDemo
                 },{
-                    path:'button',
-                    component:ButtonDemo
+                    path: 'button',
+                    component: ButtonDemo
                 },{
-                    path:'dialog',
-                    component:DialogDemo
+                    path: 'dialog',
+                    component: DialogDemo
                 },{
-                    path:'tabs',
-                    component:TabsDemo
+                    path: 'tabs',
+                    component: TabsDemo
                 }
             ]
         }
